c7511951e69fe2d7056a9e340d4d5d5ede33a264
peachberry <peachberry0318@gmail.com>
2020-08-05 18:51:31 +0900

    new StackView_Practice

---
+++ StackView_Practice/StackView_Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView_Practice
+//
+//  Created by 백승화 on 2020/08/05.
+//  Copyright © 2020 백승화. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
