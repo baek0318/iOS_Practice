65a1a5f80a22acfdfcefb189feec9b81708c1c71
peachberry <peachberry0318@gmail.com>
2020-12-08 23:09:34 +0900

    add gitignore

---
+++ Scheduler/Scheduler/ScheduleCalendar/ScheduleCalendarModel.swift
@@ -0,0 +1,8 @@
+//
+//  ScheduleCalendarModel.swift
+//  Scheduler
+//
+//  Created by 백승화 on 2020/12/08.
+//
+
+import Foundation
---
+++ Scheduler/Scheduler/ScheduleCalendar/ScheduleCalendarModelProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ScheduleCalendarModelProtocol.swift
+//  Scheduler
+//
+//  Created by 백승화 on 2020/12/08.
+//
+
+import Foundation
